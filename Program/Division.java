24032015d6b617445c7d46bee77171487176c8b1
deep misra <deepakmishra93152@gmail.com>
2024-11-21 21:24:07 +0530

    Devision of numbers

---
+++ Program/Division.java
@@ -0,0 +1,10 @@
+class Division {
+    public static void main(String[] args) {
+        
+        System.out.println();
+        int a = 24 , b = 8 ;
+        int c = a / b;
+        System.out.println("Devision of boths number is :" + " " + c);
+    }
+}
+
