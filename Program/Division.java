4e6738ba9f2e2847be7edfcb5547e08cc77a7389
deep misra <deepakmishra93152@gmail.com>
2024-11-21 21:44:17 +0530

    Chnges

--- Program/Division.java
+++ Program/Division.java
@@ -2,7 +2,7 @@ class Division {
     public static void main(String[] args) {
         
         System.out.println();
-        int a = 24 , b = 8 ;
+        int a = 40 , b = 8 ;
         int c = a / b;
         System.out.println("Devision of boths number is :" + " " + c);
     }
---
+++ Program/Equation.java
@@ -0,0 +1,7 @@
+ class Equation {
+    public static void main(String[] args) {
+        int x= 2 , y = 3 , z ;
+        z = x*x + y*y + 2*x*y;
+        System.out.println(z);
+ } 
+}
