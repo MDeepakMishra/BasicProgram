e6f35d8e7a5c60431321494d537c22e1c4f90439
deep misra <deepakmishra93152@gmail.com>
2024-11-21 20:51:08 +0530

    Initial commit

--- Program/Addition.java
+++ Program/Addition.java
@@ -4,11 +4,12 @@ class Addition {
     public static void main(String[] args) {
         System.out.println("Enter THe Numbers ::");
         System.out.println();
-        Scanner sc = new Scanner(System.in);
-       // int a = sc.nextInt();
-        //int b = sc.nextInt();
+       // @SuppressWarnings("resource")
+        //Scanner sc = new Scanner(System.in);
+     //  int a = sc.nextInt();
+       //int b = sc.nextInt();
         
-        int a = 2 , b = 8 ;
+       int a = 2 , b = 8 ;
         int c = a + b;
         System.out.println("Addition of boths number is :" + " " + c);
     }
