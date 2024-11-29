ecf4dd072113cf6d25208516fd435545bb0157fe
deep misra <deepakmishra93152@gmail.com>
2024-11-29 20:45:23 +0530

    Reverse Number

--- Program/Reverse.java
+++ Program/Reverse.java
@@ -1,16 +1,18 @@
- class Reverse {
-   public static void main(String[] args) {
-    System.out.println("BEFORE REVERSE");
-    int n = 1234 , rem , rev = 0 ;
-    System.out.println(n);
+class Reverse {
+  public static void main(String[] args) {
+      System.out.println("BEFORE REVERSE");
+      int n = 1234, rem, rev = 0;
+      System.out.println(n);
 
-    while(n >= 0)
-    {
-        rem = n % 10 ;
-        rev = rev * 10 + rem ;
-        n = n / 10 ;
-    }
-   }   
-  System.out.println("After REVERSE");
-  // System.out.println(rev);
+      // Reverse logic
+      while (n > 0) {
+          rem = n % 10;
+          rev = rev * 10 + rem;
+          n = n / 10;
+      }
+
+      // Output after reversing
+      System.out.println("AFTER REVERSE");
+      System.out.println(rev);
+  }
 }
