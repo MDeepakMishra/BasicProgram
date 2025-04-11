5f16b7f5202c78d04dff08d219fddcc88ab29530
deep misra <deepakmishra93152@gmail.com>
2025-04-11 12:34:09 +0530

    initial commit

---
+++ Division.java
@@ -0,0 +1,11 @@
+package BasicProgram;
+class Division {
+    public static void main(String[] args) {
+        
+        System.out.println();
+        int a = 40 , b = 8 ;
+        int c = a / b;
+        System.out.println("Devision of boths number is :" + " " + c);
+    }
+}
+
