22401e2f41acd3476bb6d1ed9c0383872ceb2f7a
deep misra <deepakmishra93152@gmail.com>
2025-04-11 12:27:29 +0530

    Addition Program

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package BasicProgram;
+import java.util.Scanner;
+
+class Addition {
+    public static void main(String[] args) {
+        System.out.println("Enter THe Numbers ::");
+        System.out.println();
+       // @SuppressWarnings("resource")
+        //Scanner sc = new Scanner(System.in);
+     //  int a = sc.nextInt();
+       //int b = sc.nextInt();
+        
+       int a = 2 , b = 8 ;
+        int c = a + b;
+        System.out.println("Addition of boths number is :" + " " + c);
+    }
+}
