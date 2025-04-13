1ba9ec4df8c4dcaeaa1e6f951d79c474b5d3937b
deep misra <deepakmishra93152@gmail.com>
2025-04-13 21:24:47 +0530

    Substraction

---
+++ Subtract.java
@@ -0,0 +1,10 @@
+
+class Subtract {
+    public static void main(String[] args) {
+        
+        System.out.println();
+        int a = 2 , b = 8 ;
+        int c = a - b;
+        System.out.println("Subtract of boths number is :" + " " + c);
+    }
+}
