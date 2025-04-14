7c45752a38488cff367d96403d44b29249f08ea4
deep misra <deepakmishra93152@gmail.com>
2025-04-14 12:42:14 +0530

    find Value of any eqations

---
+++ Equation.java
@@ -0,0 +1,8 @@
+package BasicProgram;
+ class Equation {
+    public static void main(String[] args) {
+        int x= 2 , y = 3 , z ;
+        z = x*x + y*y + 2*x*y;
+        System.out.println(z);
+ } 
+}
