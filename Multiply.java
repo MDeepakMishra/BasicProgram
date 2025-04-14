efd5c5cb8764f5c427d33c00cb1c4771272fd213
deep misra <deepakmishra93152@gmail.com>
2025-04-14 12:44:29 +0530

    Multiply the numbers

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+package BasicProgram;
+class Multiply {
+    public static void main(String[] args) {
+        
+        System.out.println();
+        int a = 2 , b = 8 ;
+        int c = a * b;
+        System.out.println("Multiplication of boths number is :" + " " + c);
+    }
+}
+
